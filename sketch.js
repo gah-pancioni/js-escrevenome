cfae6b5332b98aa681aad230bc2493cc5e42a185
gah-pancioni <gabriel.pancioni.elias@escola.pr.gov.br>
2024-08-14 12:31:15 -0300

    adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+    createCanvas(600, 600);
+    background("black");
+}
+
+function drawn() {
+    stronke("blue");
+    fill("red");
+
+    if(mouseIsPressed){
+        react(mouseX, mouse Y, 20, 35);
+    }
+
+  }
+  
